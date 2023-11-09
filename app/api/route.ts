664818e88f7e670b60ac54ae7467d78a5e58bd48
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-11-09 16:31:27 +0200

    Small improvement.

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,7 @@
 "use server";
 
-import { kv } from '@vercel/kv';
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const data = await kv.scan(0);
-  return NextResponse.json(data[1]);
+  return NextResponse.json({});
 }
