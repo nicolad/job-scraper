9e2a0015c0809091310c6648e167a764e442b9db
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-11-22 17:58:45 +0200

    Small improvement.

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
+import cron from "node-cron";
+import axios from "axios";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -18,4 +20,28 @@ app.prepare().then(() => {
       dev ? "development" : process.env.NODE_ENV
     }`
   );
+
+  const scrapeTask = async () => {
+    console.log("Running scrape task");
+    try {
+      const response = await axios.get(`http://localhost:${port}/api/scrape`);
+      console.log("Scrape task completed:", response.data);
+    } catch (error) {
+      console.error("Error during scrape task:", error);
+    }
+  };
+
+  const enrichTask = async () => {
+    console.log("Running scrape task");
+    try {
+      const response = await axios.get(`http://localhost:${port}/api/enrich`);
+      console.log("Scrape task completed:", response.data);
+    } catch (error) {
+      console.error("Error during scrape task:", error);
+    }
+  };
+
+  cron.schedule("*/30 * * * *", scrapeTask);
+  cron.schedule("*/1 * * * *", scrapeTask);
+  cron.schedule("*/1 * * * *", enrichTask);
 });
